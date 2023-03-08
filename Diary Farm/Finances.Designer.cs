e6935c109063caebdbf8e2c541858b246bed935a
eslaamm66 <you@example.com>
2023-03-08 20:15:32 +0200

    change page size

--- Diary Farm/Finances.Designer.cs
+++ Diary Farm/Finances.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Finances
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1253, 871);
+            this.Name = "Finances";
             this.Text = "Finances";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
