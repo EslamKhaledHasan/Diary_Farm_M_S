3d18d276af7465bc58d5fe400822392b9470cb03
eslaamm66 <you@example.com>
2023-03-08 17:15:25 +0200

    change label text

--- Diary Farm/Login.Designer.cs
+++ Diary Farm/Login.Designer.cs
@@ -38,26 +38,27 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(284, 593);
+            this.panel1.Size = new System.Drawing.Size(320, 519);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Copperplate Gothic Light", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(355, 36);
+            this.label1.Location = new System.Drawing.Point(547, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(335, 30);
+            this.label1.Size = new System.Drawing.Size(91, 30);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Dairy Farm Software";
+            this.label1.Text = "D F S";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 593);
+            this.ClientSize = new System.Drawing.Size(900, 519);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Copperplate Gothic Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
