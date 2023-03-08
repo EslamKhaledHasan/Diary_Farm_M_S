dfcc3033818dbc017fc937121aac7cc0853b31f5
eslaamm66 <you@example.com>
2023-03-08 17:12:48 +0200

    change panel color

--- Diary Farm/Login.Designer.cs
+++ Diary Farm/Login.Designer.cs
@@ -33,6 +33,7 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
